acc497ed35bf5097eada0c6dfe90e3fcb65dda20
sas234 <120399555+sas234@users.noreply.github.com>
2024-09-16 02:46:56 +0530

    Add files via upload

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'> 
+      <div className="header-contents">
+      <h2>Order Your Favourite Food Here</h2>
+      <p>Welcome to our food ordering service! Explore a wide range of delicious meals and have them delivered straight to your door. Whether you're craving a hearty meal, a light snack, or a sweet treat, we have something for everyone. Start your culinary adventure with us today!</p>
+      <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
